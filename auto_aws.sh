b333fd8f25412434ea75ab6007a862e4e7df8492
Nixk01 <nikunjjadhav786@gmail.com>
2025-03-27 06:10:01 +0530

    Auto commit on 2025-03-27 06:10:01

--- auto_aws.sh
+++ auto_aws.sh
@@ -10,4 +10,4 @@ git add .
 git commit -m "Auto commit on $(date '+%Y-%m-%d %H:%M:%S')"
 
 # Push to origin main (change 'main' to 'master' or your branch name if needed)
-git push origin main
+git push -ff origin main
